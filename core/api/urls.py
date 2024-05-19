cc4a9892a7890147adad0b5cb437f2090d139f14
Tonmecool <qpwoei.86@mail.ru>
2024-05-19 15:22:58 +0700

    add products api router

--- core/api/urls.py
+++ core/api/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from ninja import NinjaAPI
 
 from core.api.schemas import PingResponseSchema
+from core.api.v1.urls import router as v1_router
 
 
 api = NinjaAPI()
@@ -13,6 +14,8 @@ api = NinjaAPI()
 def ping(request: HttpRequest) -> PingResponseSchema:
     return PingResponseSchema(result=True)
 
+
+api.add_router('v1/', v1_router)
 
 urlpatterns = [
     path("", api.urls),
---
+++ core/api/v1/products/schemas.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class ProductSchema(BaseModel):
+    title: str
+    description: str
+    created_at: datetime
+    updated_at: datetime | None = None
+
+
+ProductListSchema = list[ProductSchema]
